95c4f10bc8ad87f25daa2fac8e3e66b80be9c8b3
Felipe Soto C <pipesoto13@gmail.com>
2020-10-20 20:38:00 -0500

    changes

--- src/App.js
+++ src/App.js
@@ -2,14 +2,31 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      value: 0
+    };
+
+    this.updateNum = this.updateNum.bind(this);
+  }
+
+  updateNum(){
+    this.setState({
+      value: this.state.value + 1
+    })
+  }
+  
   render() {
     return (
       <div>
-        <span className="value">0</span>
-        <button id="inc">Incrementa</button>
+        <span className="value">{this.state.value}</span>
+        <button id="inc" onClick={this.updateNum}>Incrementa</button>
       </div>
     );
   }
+
 }
 
 export default App;
